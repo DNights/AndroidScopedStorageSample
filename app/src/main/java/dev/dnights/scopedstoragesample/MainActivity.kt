b40c9c6d96dfc2355744b167b5b92fa1cd0a02c8
hyun dong han <guehgk456@gmail.com>
2019-12-10 22:36:17 +0900

    add button_move_media_store_file

--- app/src/main/java/dev/dnights/scopedstoragesample/MainActivity.kt
+++ app/src/main/java/dev/dnights/scopedstoragesample/MainActivity.kt
@@ -1,11 +1,18 @@
 package dev.dnights.scopedstoragesample
 
+import android.content.Intent
 import android.os.Bundle
+import dev.dnights.scopedstoragesample.mediastore.MediaStoreFileActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button_move_media_store_file.setOnClickListener {
+            startActivity(Intent(this@MainActivity, MediaStoreFileActivity::class.java))
+        }
     }
 }
