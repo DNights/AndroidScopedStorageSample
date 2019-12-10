a49ef17e4a7770daba0413f3ff923ed530a9af51
hyun dong han <guehgk456@gmail.com>
2019-12-10 22:32:03 +0900

    Add MediaStoreFileActivity

---
+++ app/src/main/java/dev/dnights/scopedstoragesample/mediastore/MediaStoreFileActivity.kt
@@ -0,0 +1,13 @@
+package dev.dnights.scopedstoragesample.mediastore
+
+import android.os.Bundle
+import dev.dnights.scopedstoragesample.BaseActivity
+import dev.dnights.scopedstoragesample.R
+
+class MediaStoreFileActivity : BaseActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_media_store_file)
+    }
+}
