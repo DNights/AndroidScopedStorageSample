31969fbda20ff69cd69303487f04d354ad9a0bc3
hyundonghan <guehgk456@gmail.com>
2019-12-11 18:02:30 +0900

    add button_move_storage_access_framework

--- app/src/main/java/dev/dnights/scopedstoragesample/MainActivity.kt
+++ app/src/main/java/dev/dnights/scopedstoragesample/MainActivity.kt
@@ -2,6 +2,7 @@ package dev.dnights.scopedstoragesample
 
 import android.content.Intent
 import android.os.Bundle
+import dev.dnights.scopedstoragesample.SAF.StorageAccessFrameworkActivity
 import dev.dnights.scopedstoragesample.mediastore.MediaStoreFileActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -13,6 +14,10 @@ class MainActivity : BaseActivity() {
 
         button_move_media_store_file.setOnClickListener {
             startActivity(Intent(this@MainActivity, MediaStoreFileActivity::class.java))
+        }
+
+        button_move_storage_access_framework.setOnClickListener {
+            startActivity(Intent(this@MainActivity, StorageAccessFrameworkActivity::class.java))
         }
     }
 }
